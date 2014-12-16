60fba7da5f0ebd252997409aa7336a048870d567
Ilya <bound1ess.dev@gmail.com>
2014-12-16 22:03:17 +0600

    Working on the create command.

--- peb.py
+++ peb.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 PEB_VERSION = "0.0.0"
 
-from os.path import isfile
+from os.path import isfile, exists, dirname, abspath
+from os import mkdir
 from cement.core import foundation, controller
 
 def phpcpp_is_installed():
@@ -9,10 +10,21 @@ def phpcpp_is_installed():
 	and isfile("/usr/lib/libphpcpp.a") \
 	and isfile("/usr/lib/libphpcpp.so")
 
+def create_extension_dir(dir):
+	path = "%s/%s/" % (dirname(abspath(__file__)), dir)
+	if exists(path):
+		return False
+	else:	
+		mkdir(path)
+		return True
+
 class PebBaseController(controller.CementBaseController):
 	class Meta:
 		label = "base"
 		description = "PHP Extension Builder"	
+		arguments = [
+			(['--name'], dict(action = 'store', dest = 'name', help = 'Extension name.'))
+		]
 
 	@controller.expose(hide = True)
 	def default(self):
@@ -24,6 +36,13 @@ class PebBaseController(controller.CementBaseController):
 			print("PHP-CPP is correctly installed.")
 		else:
 			print("PHP-CPP is not installed on your machine.")
+
+	@controller.expose(help = "Create new PHP-CPP project.")
+	def create(self):
+		if create_extension_dir(self.app.pargs.name):
+			print("Directory successfully created.")
+		else:
+			print("Directory already exists.")
 
 class Peb(foundation.CementApp):
 	class Meta:
